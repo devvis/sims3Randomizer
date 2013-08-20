63bfb315994185f44c6f75aa1fa95bdfd5abda3c
Gustav Eklundh <gustav@xcoders.info>
2013-08-20 06:12:53 +0200

    Added generate-button to the layout

--- theSims3Randomizer/mainWnd.Designer.cs
+++ theSims3Randomizer/mainWnd.Designer.cs
@@ -46,6 +46,7 @@
             this.sim1Trait1 = new System.Windows.Forms.Label();
             this.sim1Career = new System.Windows.Forms.Label();
             this.sim1Gender = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
             this.sim1Static.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -244,11 +245,21 @@
             this.sim1Gender.Size = new System.Drawing.Size(0, 13);
             this.sim1Gender.TabIndex = 8;
             // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(542, 387);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 9;
+            this.btnGenerate.Text = "&Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            // 
             // mainWnd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(629, 422);
+            this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.sim1Static);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -282,6 +293,7 @@
         private System.Windows.Forms.Label sim1Trait1;
         private System.Windows.Forms.Label sim1Career;
         private System.Windows.Forms.Label sim1Gender;
+        private System.Windows.Forms.Button btnGenerate;
     }
 }
 

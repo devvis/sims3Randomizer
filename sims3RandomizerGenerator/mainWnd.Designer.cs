eecdf1f6c1227c36c0fed2681232a24a850b67e3
Gustav Eklundh <gustav@xcoders.info>
2013-08-21 00:19:40 +0200

    Added a draft for an challenge-creator!

---
+++ sims3RandomizerGenerator/mainWnd.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace sims3RandomizerGenerator
+{
+    partial class mainWnd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWnd));
+            this.simNumLabel = new System.Windows.Forms.Label();
+            this.simSaveButton = new System.Windows.Forms.Button();
+            this.simGenderStatic = new System.Windows.Forms.Label();
+            this.simCareerStatic = new System.Windows.Forms.Label();
+            this.simAgeStatic = new System.Windows.Forms.Label();
+            this.simWishStatic = new System.Windows.Forms.Label();
+            this.simTypeStatic = new System.Windows.Forms.Label();
+            this.simTrait1Static = new System.Windows.Forms.Label();
+            this.simTrait2Static = new System.Windows.Forms.Label();
+            this.simTrait3Static = new System.Windows.Forms.Label();
+            this.simTrait4Static = new System.Windows.Forms.Label();
+            this.simTrait5Static = new System.Windows.Forms.Label();
+            this.simSelectionList = new System.Windows.Forms.ComboBox();
+            this.simGender = new System.Windows.Forms.ComboBox();
+            this.simJob = new System.Windows.Forms.ComboBox();
+            this.simAge = new System.Windows.Forms.ComboBox();
+            this.simLifetime = new System.Windows.Forms.ComboBox();
+            this.simType = new System.Windows.Forms.ComboBox();
+            this.simTrait1 = new System.Windows.Forms.ComboBox();
+            this.simTrait2 = new System.Windows.Forms.ComboBox();
+            this.simTrait3 = new System.Windows.Forms.ComboBox();
+            this.simTrait4 = new System.Windows.Forms.ComboBox();
+            this.simTrait5 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // simNumLabel
+            // 
+            this.simNumLabel.AutoSize = true;
+            this.simNumLabel.Location = new System.Drawing.Point(12, 15);
+            this.simNumLabel.Name = "simNumLabel";
+            this.simNumLabel.Size = new System.Drawing.Size(60, 13);
+            this.simNumLabel.TabIndex = 1;
+            this.simNumLabel.Text = "Select Sim:";
+            // 
+            // simSaveButton
+            // 
+            this.simSaveButton.Location = new System.Drawing.Point(273, 356);
+            this.simSaveButton.Name = "simSaveButton";
+            this.simSaveButton.Size = new System.Drawing.Size(95, 23);
+            this.simSaveButton.TabIndex = 2;
+            this.simSaveButton.Text = "Save Challenge";
+            this.simSaveButton.UseVisualStyleBackColor = true;
+            // 
+            // simGenderStatic
+            // 
+            this.simGenderStatic.AutoSize = true;
+            this.simGenderStatic.Location = new System.Drawing.Point(12, 64);
+            this.simGenderStatic.Name = "simGenderStatic";
+            this.simGenderStatic.Size = new System.Drawing.Size(45, 13);
+            this.simGenderStatic.TabIndex = 3;
+            this.simGenderStatic.Text = "Gender:";
+            // 
+            // simCareerStatic
+            // 
+            this.simCareerStatic.AutoSize = true;
+            this.simCareerStatic.Location = new System.Drawing.Point(12, 91);
+            this.simCareerStatic.Name = "simCareerStatic";
+            this.simCareerStatic.Size = new System.Drawing.Size(41, 13);
+            this.simCareerStatic.TabIndex = 4;
+            this.simCareerStatic.Text = "Career:";
+            // 
+            // simAgeStatic
+            // 
+            this.simAgeStatic.AutoSize = true;
+            this.simAgeStatic.Location = new System.Drawing.Point(12, 118);
+            this.simAgeStatic.Name = "simAgeStatic";
+            this.simAgeStatic.Size = new System.Drawing.Size(29, 13);
+            this.simAgeStatic.TabIndex = 5;
+            this.simAgeStatic.Text = "Age:";
+            // 
+            // simWishStatic
+            // 
+            this.simWishStatic.AutoSize = true;
+            this.simWishStatic.Location = new System.Drawing.Point(12, 145);
+            this.simWishStatic.Name = "simWishStatic";
+            this.simWishStatic.Size = new System.Drawing.Size(34, 13);
+            this.simWishStatic.TabIndex = 6;
+            this.simWishStatic.Text = "Wish:";
+            // 
+            // simTypeStatic
+            // 
+            this.simTypeStatic.AutoSize = true;
+            this.simTypeStatic.Location = new System.Drawing.Point(12, 172);
+            this.simTypeStatic.Name = "simTypeStatic";
+            this.simTypeStatic.Size = new System.Drawing.Size(34, 13);
+            this.simTypeStatic.TabIndex = 7;
+            this.simTypeStatic.Text = "Type:";
+            // 
+            // simTrait1Static
+            // 
+            this.simTrait1Static.AutoSize = true;
+            this.simTrait1Static.Location = new System.Drawing.Point(12, 199);
+            this.simTrait1Static.Name = "simTrait1Static";
+            this.simTrait1Static.Size = new System.Drawing.Size(40, 13);
+            this.simTrait1Static.TabIndex = 8;
+            this.simTrait1Static.Text = "Trait 1:";
+            // 
+            // simTrait2Static
+            // 
+            this.simTrait2Static.AutoSize = true;
+            this.simTrait2Static.Location = new System.Drawing.Point(12, 226);
+            this.simTrait2Static.Name = "simTrait2Static";
+            this.simTrait2Static.Size = new System.Drawing.Size(40, 13);
+            this.simTrait2Static.TabIndex = 9;
+            this.simTrait2Static.Text = "Trait 2:";
+            // 
+            // simTrait3Static
+            // 
+            this.simTrait3Static.AutoSize = true;
+            this.simTrait3Static.Location = new System.Drawing.Point(12, 253);
+            this.simTrait3Static.Name = "simTrait3Static";
+            this.simTrait3Static.Size = new System.Drawing.Size(40, 13);
+            this.simTrait3Static.TabIndex = 10;
+            this.simTrait3Static.Text = "Trait 3:";
+            // 
+            // simTrait4Static
+            // 
+            this.simTrait4Static.AutoSize = true;
+            this.simTrait4Static.Location = new System.Drawing.Point(12, 280);
+            this.simTrait4Static.Name = "simTrait4Static";
+            this.simTrait4Static.Size = new System.Drawing.Size(40, 13);
+            this.simTrait4Static.TabIndex = 11;
+            this.simTrait4Static.Text = "Trait 4:";
+            // 
+            // simTrait5Static
+            // 
+            this.simTrait5Static.AutoSize = true;
+            this.simTrait5Static.Location = new System.Drawing.Point(12, 307);
+            this.simTrait5Static.Name = "simTrait5Static";
+            this.simTrait5Static.Size = new System.Drawing.Size(40, 13);
+            this.simTrait5Static.TabIndex = 12;
+            this.simTrait5Static.Text = "Trait 5:";
+            // 
+            // simSelectionList
+            // 
+            this.simSelectionList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simSelectionList.FormattingEnabled = true;
+            this.simSelectionList.Items.AddRange(new object[] {
+            "Sim 1",
+            "Sim 2",
+            "Sim 3",
+            "Sim 4",
+            "Sim 5",
+            "Sim 6",
+            "Sim 7",
+            "Sim 8"});
+            this.simSelectionList.Location = new System.Drawing.Point(78, 12);
+            this.simSelectionList.Name = "simSelectionList";
+            this.simSelectionList.Size = new System.Drawing.Size(121, 21);
+            this.simSelectionList.TabIndex = 13;
+            // 
+            // simGender
+            // 
+            this.simGender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simGender.FormattingEnabled = true;
+            this.simGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.simGender.Location = new System.Drawing.Point(78, 61);
+            this.simGender.Name = "simGender";
+            this.simGender.Size = new System.Drawing.Size(283, 21);
+            this.simGender.TabIndex = 14;
+            // 
+            // simJob
+            // 
+            this.simJob.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simJob.FormattingEnabled = true;
+            this.simJob.Items.AddRange(new object[] {
+            "Steve",
+            "Jobs"});
+            this.simJob.Location = new System.Drawing.Point(78, 88);
+            this.simJob.Name = "simJob";
+            this.simJob.Size = new System.Drawing.Size(283, 21);
+            this.simJob.TabIndex = 15;
+            // 
+            // simAge
+            // 
+            this.simAge.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simAge.FormattingEnabled = true;
+            this.simAge.Items.AddRange(new object[] {
+            "Toddler",
+            "Child",
+            "Teen",
+            "Young Adult",
+            "Adult",
+            "Elder"});
+            this.simAge.Location = new System.Drawing.Point(78, 115);
+            this.simAge.Name = "simAge";
+            this.simAge.Size = new System.Drawing.Size(283, 21);
+            this.simAge.TabIndex = 16;
+            // 
+            // simLifetime
+            // 
+            this.simLifetime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simLifetime.FormattingEnabled = true;
+            this.simLifetime.Items.AddRange(new object[] {
+            "I",
+            "Wish",
+            "For",
+            "A",
+            "Lot",
+            "Off",
+            "Stuff"});
+            this.simLifetime.Location = new System.Drawing.Point(78, 142);
+            this.simLifetime.Name = "simLifetime";
+            this.simLifetime.Size = new System.Drawing.Size(283, 21);
+            this.simLifetime.TabIndex = 17;
+            // 
+            // simType
+            // 
+            this.simType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simType.FormattingEnabled = true;
+            this.simType.Items.AddRange(new object[] {
+            "Human",
+            "Fairy",
+            "Werewolf"});
+            this.simType.Location = new System.Drawing.Point(78, 169);
+            this.simType.Name = "simType";
+            this.simType.Size = new System.Drawing.Size(283, 21);
+            this.simType.TabIndex = 18;
+            // 
+            // simTrait1
+            // 
+            this.simTrait1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simTrait1.FormattingEnabled = true;
+            this.simTrait1.Items.AddRange(new object[] {
+            "Trait"});
+            this.simTrait1.Location = new System.Drawing.Point(78, 196);
+            this.simTrait1.Name = "simTrait1";
+            this.simTrait1.Size = new System.Drawing.Size(283, 21);
+            this.simTrait1.TabIndex = 19;
+            // 
+            // simTrait2
+            // 
+            this.simTrait2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simTrait2.FormattingEnabled = true;
+            this.simTrait2.Items.AddRange(new object[] {
+            "Trait"});
+            this.simTrait2.Location = new System.Drawing.Point(78, 223);
+            this.simTrait2.Name = "simTrait2";
+            this.simTrait2.Size = new System.Drawing.Size(283, 21);
+            this.simTrait2.TabIndex = 20;
+            // 
+            // simTrait3
+            // 
+            this.simTrait3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simTrait3.FormattingEnabled = true;
+            this.simTrait3.Items.AddRange(new object[] {
+            "Trait"});
+            this.simTrait3.Location = new System.Drawing.Point(78, 250);
+            this.simTrait3.Name = "simTrait3";
+            this.simTrait3.Size = new System.Drawing.Size(283, 21);
+            this.simTrait3.TabIndex = 21;
+            // 
+            // simTrait4
+            // 
+            this.simTrait4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simTrait4.FormattingEnabled = true;
+            this.simTrait4.Items.AddRange(new object[] {
+            "Traits"});
+            this.simTrait4.Location = new System.Drawing.Point(78, 277);
+            this.simTrait4.Name = "simTrait4";
+            this.simTrait4.Size = new System.Drawing.Size(283, 21);
+            this.simTrait4.TabIndex = 22;
+            // 
+            // simTrait5
+            // 
+            this.simTrait5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.simTrait5.FormattingEnabled = true;
+            this.simTrait5.Items.AddRange(new object[] {
+            "Traits"});
+            this.simTrait5.Location = new System.Drawing.Point(78, 304);
+            this.simTrait5.Name = "simTrait5";
+            this.simTrait5.Size = new System.Drawing.Size(283, 21);
+            this.simTrait5.TabIndex = 23;
+            // 
+            // mainWnd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 391);
+            this.Controls.Add(this.simTrait5);
+            this.Controls.Add(this.simTrait4);
+            this.Controls.Add(this.simTrait3);
+            this.Controls.Add(this.simTrait2);
+            this.Controls.Add(this.simTrait1);
+            this.Controls.Add(this.simType);
+            this.Controls.Add(this.simLifetime);
+            this.Controls.Add(this.simAge);
+            this.Controls.Add(this.simJob);
+            this.Controls.Add(this.simGender);
+            this.Controls.Add(this.simSelectionList);
+            this.Controls.Add(this.simTrait5Static);
+            this.Controls.Add(this.simTrait4Static);
+            this.Controls.Add(this.simTrait3Static);
+            this.Controls.Add(this.simTrait2Static);
+            this.Controls.Add(this.simTrait1Static);
+            this.Controls.Add(this.simTypeStatic);
+            this.Controls.Add(this.simWishStatic);
+            this.Controls.Add(this.simAgeStatic);
+            this.Controls.Add(this.simCareerStatic);
+            this.Controls.Add(this.simGenderStatic);
+            this.Controls.Add(this.simSaveButton);
+            this.Controls.Add(this.simNumLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainWnd";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Sims 3 Randomizer Challenge Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label simNumLabel;
+        private System.Windows.Forms.Button simSaveButton;
+        private System.Windows.Forms.Label simGenderStatic;
+        private System.Windows.Forms.Label simCareerStatic;
+        private System.Windows.Forms.Label simAgeStatic;
+        private System.Windows.Forms.Label simWishStatic;
+        private System.Windows.Forms.Label simTypeStatic;
+        private System.Windows.Forms.Label simTrait1Static;
+        private System.Windows.Forms.Label simTrait2Static;
+        private System.Windows.Forms.Label simTrait3Static;
+        private System.Windows.Forms.Label simTrait4Static;
+        private System.Windows.Forms.Label simTrait5Static;
+        private System.Windows.Forms.ComboBox simSelectionList;
+        private System.Windows.Forms.ComboBox simGender;
+        private System.Windows.Forms.ComboBox simJob;
+        private System.Windows.Forms.ComboBox simAge;
+        private System.Windows.Forms.ComboBox simLifetime;
+        private System.Windows.Forms.ComboBox simType;
+        private System.Windows.Forms.ComboBox simTrait1;
+        private System.Windows.Forms.ComboBox simTrait2;
+        private System.Windows.Forms.ComboBox simTrait3;
+        private System.Windows.Forms.ComboBox simTrait4;
+        private System.Windows.Forms.ComboBox simTrait5;
+    }
+}
+
---
+++ sims3RandomizerGenerator/mainWnd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sims3RandomizerGenerator
+{
+    public partial class mainWnd : Form
+    {
+        public mainWnd()
+        {
+            InitializeComponent();
+        }
+    }
+}
